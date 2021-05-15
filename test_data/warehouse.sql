aabca73e33739fd6e74039aa78cba853c5093274
ibozsik94 <32265712+ibozsik94@users.noreply.github.com>
2021-05-15 12:03:21 +0200

    warehouse.sql módosítása

--- test_data/warehouse.sql
+++ test_data/warehouse.sql
@@ -8,3 +8,8 @@ INSERT INTO `restaurant`.`warehouse` (`prodid`, `prod_count`, `prod_name`, `ware
 INSERT INTO `restaurant`.`warehouse` (`prodid`, `prod_count`, `prod_name`, `warehouse_name`) VALUES ('8', '9', 'Parasztos sertésszelet petrezselymes burgonyával', 'Tengeri Raktár');
 INSERT INTO `restaurant`.`warehouse` (`prodid`, `prod_count`, `prod_name`, `warehouse_name`) VALUES ('9', '14', 'Sertéspörkölt galuskával', 'Tengeri Raktár');
 INSERT INTO `restaurant`.`warehouse` (`prodid`, `prod_count`, `prod_name`, `warehouse_name`) VALUES ('10', '13', 'Sertésszelet pirított gombával, párolt rizzsel', 'Tengeri Raktár');
+INSERT INTO `restaurant`.`warehouse` (`prodid`, `prod_count`, `prod_name`, `warehouse_name`) VALUES ('11', '3', 'Juhtúrós sztrapacska', 'Tengeri Raktár');
+INSERT INTO `restaurant`.`warehouse` (`prodid`, `prod_count`, `prod_name`, `warehouse_name`) VALUES ('12', '4', 'Cordon bleu vegyes körettel', 'Tengeri Raktár');
+INSERT INTO `restaurant`.`warehouse` (`prodid`, `prod_count`, `prod_name`, `warehouse_name`) VALUES ('13', '7', 'Hagymás rostélyos hasábburgonyával', 'Tengeri Raktár');
+INSERT INTO `restaurant`.`warehouse` (`prodid`, `prod_count`, `prod_name`, `warehouse_name`) VALUES ('14', '11', 'Csőben sült brokkoli', 'Tengeri Raktár');
+INSERT INTO `restaurant`.`warehouse` (`prodid`, `prod_count`, `prod_name`, `warehouse_name`) VALUES ('15', '2', 'Túrós csusza tepertővel', 'Tengeri Raktár');
